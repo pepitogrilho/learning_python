41c5157a85b37ea5bccce5abe590384d6658318a
pepitogrilho <pepitogrilho@gmail.com>
2020-11-19 23:34:04 +0100

    We are working on the xlwings course

--- basics_SoloLearn/FLUX_if_elif_else_for_while/if_else_02_one_line.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-
-#One-line if     ....................................
-number = 6
-even_odd = "even" if number%2 == 0 else "odd"
-print(str(number) + " is " + even_odd)
